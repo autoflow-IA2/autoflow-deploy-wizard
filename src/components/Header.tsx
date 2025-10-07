e8125786c16942c0b6d8f45f66ae12520fef1ff1
Yuri Souza <yurisouzaa2@gmail.com>
2025-10-07 19:25:27 -0300

    centralizando a logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,11 +4,11 @@ const Header = () => {
   return (
     <header className="w-full px-6 py-4 bg-background">
       <div className="max-w-7xl mx-auto">
-        <div className="flex items-center justify-start">
+        <div className="flex items-center justify-center">
           <img 
             src={logo} 
             alt="AutoFlow Logo" 
-            className="h-[100px] w-[100px] object-contain"
+            className="h-[200px] w-[300px] object-contain"
           />
         </div>
       </div>
